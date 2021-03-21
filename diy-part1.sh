272c33627c36020040921711ca64b1053a703d0e
yang <ysfnevercry@gmail.com>
2021-03-21 23:46:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,10 +11,7 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-# Add luci-app-xray
-git clone https://github.com/yichya/luci-app-xray.git package/luci-app-xray
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
